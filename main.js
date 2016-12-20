cd30fbc9012c96a6035dedee3fe0d9138845edad
GColtrane <coltranegary@gmail.com>
2016-12-20 21:50:25 +0000

    updated navigation

--- main.js
+++ main.js
@@ -3,15 +3,33 @@ $(function(){
     
     $(".side-tab").click(function(){
         var selected = $(".sidenav .selected");
-        var selectedId = selected.attr("id");
+        var selectedClass = selected.attr("class");
         var nav = $(".sidenav ul li a");
         var clickedTab = $(this);
+        var clickedId = $(this).attr("id");
         selected.removeClass("selected");
         clickedTab.addClass("selected");
+        $(".content-pane").fadeOut();
+        console.log("." + clickedId+"-pane");
+        $("." + clickedId + "-pane").fadeIn();
+        // selected tab has gc-navbar 
+        // the gc-navbar and add -pane
+        //fade in gc-navbar-pane
         
         
         
         
     });
+    
+    
+    
+    
+    
 })
 
+
+function load(e){
+    $(".content-pane").fadeOut();
+    
+    
+}
