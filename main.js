5095e5d073cab9946ed75dc097afc97ffd760f5b
GColtrane <coltranegary@gmail.com>
2016-12-21 03:52:45 +0000

    finalized UI

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(function(){
     
     $(".side-tab").click(function(){
-        var selected = $(".sidenav .selected");
+        var selected = $(".selected");
         var selectedClass = selected.attr("class");
         var nav = $(".sidenav ul li a");
         var clickedTab = $(this);
@@ -10,14 +10,7 @@ $(function(){
         selected.removeClass("selected");
         clickedTab.addClass("selected");
         $(".content-pane").fadeOut();
-        console.log("." + clickedId+"-pane");
         $("." + clickedId + "-pane").fadeIn();
-        // selected tab has gc-navbar 
-        // the gc-navbar and add -pane
-        //fade in gc-navbar-pane
-        
-        
-        
         
     });
     
@@ -28,8 +21,3 @@ $(function(){
 })
 
 
-function load(e){
-    $(".content-pane").fadeOut();
-    
-    
-}
